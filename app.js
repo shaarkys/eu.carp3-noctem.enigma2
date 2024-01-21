85d2fbc8c489268224677351e5dd530466ddcf4b
shaarkys <sharkys@email.cz>
2024-01-21 23:46:13 +0100

    SDK3 version + user name and password support
    SDK3 version + user name and password support

--- app.js
+++ app.js
@@ -1,147 +1,92 @@
 'use strict';
 const Homey = require('homey');
-var request = require('request');
+const axios = require('axios');
 
-// Build enigma2 Host from Config (make variables single for later use as well)
-const enigma2_ip = Homey.ManagerSettings.get('enigma2_ip');
-const enigma2_port = Homey.ManagerSettings.get('enigma2_port');
-var enigma2_host = enigma2_ip + ':' + enigma2_port;
+class Enigma2 extends Homey.App {
+    async onInit() {
+        this.log("enigma2 app started successfully");
 
-// Homey Requirement
-class enigma2 extends Homey.App {
-	onInit() {
-		this.log(Homey.__("appLogStart"));
-	}
+        this.enigma2_ip = await this.homey.settings.get('enigma2_ip');
+        this.enigma2_port = await this.homey.settings.get('enigma2_port');
+        this.enigma2_username = await this.homey.settings.get('enigma2_username');
+        this.enigma2_password = await this.homey.settings.get('enigma2_password');
+        this.enigma2_host = `${this.enigma2_ip}:${this.enigma2_port}`;
+
+        this.registerFlowCards();
+    }
+
+    async callEnigma2(call_spec) {
+        try {
+            const config = {
+                url: `http://${this.enigma2_host}/web/${call_spec}`,
+                method: 'get'
+            };
+
+            // Add authentication if username and password are provided
+            if (this.enigma2_username && this.enigma2_password) {
+                config.auth = {
+                    username: this.enigma2_username,
+                    password: this.enigma2_password
+                };
+            }
+
+            const response = await axios(config);
+            this.log('Following Call was send: ' + response.config.url + ' to ' + this.enigma2_host);
+            this.log('Previous Call was made successfully.');
+        } catch (error) {
+            this.error('Previous Call Failed! Maybe wrong Configuration?');
+            this.error(error);
+        }
+    }
+	
+    registerFlowCards() {
+        // Command Send Action
+        this.registerFlowCardAction('command_send', (args) => `remotecontrol?command=${args.command}`);
+
+        // Message Send Action
+        this.registerFlowCardAction('message_send', (args) => {
+            const message_complete = args.msg_text_full;
+            const message_split = message_complete.split("|");
+            const msg_type = message_split[0];
+            const timeout = message_split[1];
+            const msg_txt = message_split[2];
+            const msg_timeout = timeout === 0 ? "" : timeout;
+            return `message?text=${msg_txt}&type=${msg_type}&timeout=${msg_timeout}`;
+        });
+
+        // Powerstate Deep Standby Action
+        this.registerFlowCardAction('powerstate_deepstandby', () => 'powerstate?newstate=1');
+
+        // Powerstate Reboot Action
+        this.registerFlowCardAction('powerstate_reboot', () => 'powerstate?newstate=2');
+
+        // Restart Enigma2 Action
+        this.registerFlowCardAction('powerstate_restart_enigma2', () => 'powerstate?newstate=3');
+
+        // Powerstate On Action
+        this.registerFlowCardAction('powerstate_on', () => 'powerstate?newstate=4');
+
+        // Powerstate Off Action
+        this.registerFlowCardAction('powerstate_off', () => 'powerstate?newstate=5');
+
+        // Volume Set Action
+        this.registerFlowCardAction('vol_set', (args) => `vol?set=set${args.volume}`);
+
+        // Volume Mute Action
+        this.registerFlowCardAction('vol_mute', () => 'vol?set=mute');
+
+        // Volume Unmute Action
+        this.registerFlowCardAction('vol_unmute', () => 'vol?set=unmute');
+    }
+
+    registerFlowCardAction(cardName, getCallSpec) {
+        const actionCard = this.homey.flow.getActionCard(cardName);
+        actionCard.registerRunListener(async (args) => {
+            const call_spec = getCallSpec(args);
+            await this.callEnigma2(call_spec);
+            return true;
+        });
+    }
 }
 
-// Call Function action Flowcards
-async function call_enigma2 (call_spec){
-	var call_adress = ('http://'+enigma2_host+'/web/'+call_spec);
-	var options ={
-		url: call_adress,
-		headers: enigma2_host
-	};
-	console.log('Following Call was send: '+call_adress+' to '+enigma2_host);
-	function callback(error, response, body){
-		if (!error && response.statusCode == 200){
-			console.log('Previous Call was made successfully.');
-			console.log(':::::::::::::::::'+response.statusCode);
-		}
-		else {
-			console.log('Previous Call Failed! Maybe wrong Configuration?');
-		};
-	};
-	request(options, callback);	
-};
-// Control Flowcards
-// then / action Flowcards
-let sendCommandAction = new Homey.FlowCardAction('command_send');
-sendCommandAction
-  .register()
-  .registerRunListener((args) => {
-	  var call_spec = ('remotecontrol?command=' + args.command);
-	  call_enigma2(call_spec);
-    return true;
-});
-
-// Message Flowcards
-// then / action Flowcards
-let sendMessageAction = new Homey.FlowCardAction('message_send');
-sendMessageAction
-  .register()
-  .registerRunListener((args) => {
-		var message_complete = args.msg_text_full;
-		var message_split = message_complete.split("|");
-		var msg_type = message_split[0];
-		var timeout = message_split[1];
-		var msg_txt = message_split[2];
-	  if (timeout == 0) {
-			var msg_timeout = "";
-		} else {
-			var msg_timeout = timeout;
-		}
-		var call_spec = ('message?text='+msg_txt+'&type='+msg_type+'&timeout='+msg_timeout)
-	  call_enigma2(call_spec);
-    return true;
-});
-
-// then / action Flowcards
-// Deepstandby
-let powerstateONAction = new Homey.FlowCardAction('powerstate_deepstandby');
-powerstateONAction
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('powerstate?newstate=1');
-		call_enigma2(call_spec);
-		return true;
-});
-// Reboot
-let powerstateRebootAction = new Homey.FlowCardAction('powerstate_reboot');
-powerstateRebootAction
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('powerstate?newstate=2');
-		call_enigma2(call_spec);
-		return true;
-});
-// Restart Enigma2
-let powerstateReboot_e2Action = new Homey.FlowCardAction('powerstate_restart_enigma2');
-powerstateReboot_e2Action
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('powerstate?newstate=3');
-		call_enigma2(call_spec);
-		return true;
-});
-// Wakeup from Standby
-let powerstateWakeupAction = new Homey.FlowCardAction('powerstate_on');
-powerstateWakeupAction
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('powerstate?newstate=4');
-		call_enigma2(call_spec);
-		return true;
-});
-// standby
-let powerstateStandbyAction = new Homey.FlowCardAction('powerstate_off');
-powerstateStandbyAction
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('powerstate?newstate=5');
-		call_enigma2(call_spec);
-		return true;
-});
-
-// Volume Flowcards
-// then / action Flowcards
-// set Volume to X
-let volSetAction = new Homey.FlowCardAction('vol_set');
-volSetAction
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('vol?set=set'+args.volume);
-		call_enigma2(call_spec);
-		return true;
-});
-// Volume mute
-let volMuteAction = new Homey.FlowCardAction('vol_mute');
-volMuteAction
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('vol?set=mute');
-		call_enigma2(call_spec);
-		return true;
-	});
-// Volume unmute
-let volUnmuteAction = new Homey.FlowCardAction('vol_unmute');
-volUnmuteAction
-	.register()
-	.registerRunListener((args) => {
-		var call_spec = ('vol?set=down');
-		call_enigma2(call_spec);
-		var call_spec = ('vol?set=up');
-		call_enigma2(call_spec);
-		return true;
-});
-
-module.exports = enigma2;
+module.exports = Enigma2;
